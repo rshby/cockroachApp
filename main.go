f8ea85a6b83ac8cc05b92289fd189a4cab10beb8
Reo Sahobby <reoshby@gmail.com>
2024-01-21 06:41:15 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,17 +3,15 @@ package main
 import (
 	"cockroachApp/config"
 	"cockroachApp/database"
-	"encoding/json"
 	"fmt"
 )
 
 func main() {
 	// declare config app
 	cfg := config.NewConfigApp()
-	cfgJson, _ := json.Marshal(&cfg)
-	fmt.Println(string(cfgJson))
 
-	var db database.Database = database.NewMySqlDatabase(cfg)
+	// declare database
+	db := database.NewMySqlDatabase(cfg)
 	dbConn := db.GetDB()
 	fmt.Println(dbConn) // TODO : nanti dihapus
 }
